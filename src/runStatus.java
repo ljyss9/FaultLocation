bf56979e977da81ddc37b99e8b70162322f0c685
ljyss9 <ljyss9@163.com>
2017-12-14 09:30:56 +0800

    for status of project

--- src/runStatus.java
+++ src/runStatus.java
@@ -1,7 +1,52 @@
-package PACKAGE_NAME;
-
 /**
  * Created by ljy on 17-12-13.
  */
 public class runStatus {
+    private boolean runTestCase = false;
+    private boolean getTestPath = false;
+    private boolean rateTestcase = false;
+    private boolean dymaticSlice = false;
+
+    public runStatus(){
+
+    }
+
+    public runStatus(boolean a,boolean b, boolean c, boolean d){
+        runTestCase = a;
+        getTestPath = b;
+        rateTestcase = c;
+        dymaticSlice = d;
+    }
+
+    public boolean isRunTestCase() {
+        return runTestCase;
+    }
+
+    public void setRunTestCase(boolean runTestCase) {
+        this.runTestCase = runTestCase;
+    }
+
+    public boolean isGetTestPath() {
+        return getTestPath;
+    }
+
+    public void setGetTestPath(boolean getTestPath) {
+        this.getTestPath = getTestPath;
+    }
+
+    public boolean isRateTestcase() {
+        return rateTestcase;
+    }
+
+    public void setRateTestcase(boolean rateTestcase) {
+        this.rateTestcase = rateTestcase;
+    }
+
+    public boolean isDymaticSlice() {
+        return dymaticSlice;
+    }
+
+    public void setDymaticSlice(boolean dymaticSlice) {
+        this.dymaticSlice = dymaticSlice;
+    }
 }
