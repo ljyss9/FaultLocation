6af65277c0fd38c8e2ea7875b1152450a977f94d
ljyss9 <ljyss9@163.com>
2018-01-04 21:09:52 +0800

    add for shuoming

--- src/aboutFrame.java
+++ src/aboutFrame.java
@@ -1,5 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by ljy on 17-12-26.
  */
-public class intrFrame {
+public class aboutFrame {
+    private JFrame jFrame;
+    private JLabel jLabelWen;
+    private JLabel jLabelPic;
+    private Icon pic;
+    private JButton jButton;
+
+    public aboutFrame(){
+        jFrame = new JFrame("关于SIFL缺陷定位");
+        jButton = new JButton("ok");
+        String shuoming = "<html>SIFL缺陷定位工具 版本1.0<br><br>" +
+                "Copyright©LiJiaYi <br><br>" +
+                "如有任何意见或者问题，请邮件联系：<br>" +
+                "ljyss9@163.com</html>";
+        jLabelWen = new JLabel(shuoming);
+        pic = new ImageIcon("about.png");
+        jLabelPic = new JLabel(pic);
+        if(jLabelPic == null)
+        {
+            System.out.println("jLabel");
+        }
+        jLabelWen.setFont(new Font("楷体",Font.PLAIN,18));
+        jFrame.setLayout(null);
+        jLabelWen.setBounds(0,0,310,150);
+        jLabelPic.setBounds(0,160,280,230);
+        jButton.setBounds(130,415,60,30);
+        jFrame.add(jLabelWen);
+        jFrame.add(jLabelPic);
+        jFrame.add(jButton);
+        jFrame.setVisible(true);
+        jFrame.setBounds(320,320,320,480);
+        jFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        myEvent_exit();
+    }
+
+    private void myEvent_exit(){
+        jButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent arg0) {
+              jFrame.dispose();
+            }
+        });
+    }
+//    public static void main(String args[]){
+//        new aboutFrame();
+//    }
+
 }
--- src/shuomingFrame.java
+++ src/shuomingFrame.java
@@ -1,7 +1,45 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by ljy on 18-1-4.
  */
 public class shuomingFrame {
+    private JFrame jFrame;
+    private JLabel jLabelWen;
+    private JButton jButton;
+
+    public shuomingFrame(){
+        jFrame = new JFrame("使用说明");
+        jButton = new JButton("ok");
+        String shuoming = "<html>使用说明:<br>" +
+                "请首先导入程序，并按照项目分析的选项顺序执行，" +
+                "可通过结果展示查看每一步的执行结果，" +
+                "但是查看结果前，请先执行该分析。</html>";
+        jLabelWen = new JLabel(shuoming);
+        jLabelWen.setFont(new Font("楷体",Font.PLAIN,18));
+        jFrame.setLayout(null);
+        jLabelWen.setBounds(0,0,310,120);
+        jButton.setBounds(130,130,60,30);
+        jFrame.add(jLabelWen);
+        jFrame.add(jButton);
+        jFrame.setVisible(true);
+        jFrame.setBounds(350,350,320,190);
+        jFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        myEvent_exit();
+    }
+
+    private void myEvent_exit(){
+        jButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent arg0) {
+                jFrame.dispose();
+            }
+        });
+    }
+
+//    public static void main(String args[]){
+//        new shuomingFrame();
+//    }
 }
